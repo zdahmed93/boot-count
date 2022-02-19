88c24e2f2f438bd8e1e50962d0d174708c1cf1d8
Ahmed Zribi <ahmed.zribi@zeta-box.com>
2022-02-19 10:32:31 +0100

    Add the 'rounded-pill' class to the reset buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,8 +14,8 @@ export default function Counter(props) {
                 <button className="btn btn-info btn-lg m-2" onClick={props.onIncrement}>Increment +</button>
             </div>
             <div className="d-flex flex-wrap justify-content-center">
-                <button className="btn btn-outline-danger m-2 px-3" onClick={props.onReset}>Reset Count</button>
-                <button className="btn btn-outline-danger m-2 px-4" onClick={props.onResetAll}>Reset All</button>
+                <button className="btn btn-outline-danger rounded-pill m-2 px-3" onClick={props.onReset}>Reset Count</button>
+                <button className="btn btn-outline-danger rounded-pill m-2 px-4" onClick={props.onResetAll}>Reset All</button>
             </div>
         </div>
     )
